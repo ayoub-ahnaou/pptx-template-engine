cbc8c34777dd98b1cd4cf609ad89b8ccae9cb1e5
ayoub-ahnaou <ayoubahnaou@gmail.com>
2026-09-21 11:59:33 +0000

    refactor PptxShape to improve code readability by consolidating and simplifying method implementations

--- src/pptx/PptxShape.js
+++ src/pptx/PptxShape.js
@@ -1,49 +1,24 @@
 class PptxShape {
 
     constructor(xml) {
-
         this.xml = xml;
 
-        this.id =
-            this.extractAttribute(
-                "<p:cNvPr",
-                "id"
-            );
-
-        this.name =
-            this.extractAttribute(
-                "<p:cNvPr",
-                "name"
-            );
+        this.id = this.extractAttribute("<p:cNvPr", "id");
+        this.name = this.extractAttribute("<p:cNvPr", "name");
     }
 
     extractAttribute(element, attribute) {
-
-        const regex =
-            new RegExp(
-                `${element}\\s+[^>]*?${attribute}="([^"]*)"`
-            );
-
-        const match =
-            this.xml.match(regex);
-
-        return match
-            ? match[1]
-            : null;
+        const regex = new RegExp(`${element}\\s+[^>]*?${attribute}="([^"]*)"`);
+        const match = this.xml.match(regex);
+        return match ? match[1] : null;
     }
 
     getText() {
-
-        const regex =
-            /<a:t>([\s\S]*?)<\/a:t>/g;
-
+        const regex = /<a:t>([\s\S]*?)<\/a:t>/g;
         const texts = [];
-
         let match;
 
-        while (
-            (match = regex.exec(this.xml)) !== null
-        ) {
+        while ((match = regex.exec(this.xml)) !== null) {
             texts.push(match[1]);
         }
 
@@ -51,31 +26,24 @@ class PptxShape {
     }
 
     setText(text) {
+        const escapedText = this.escapeXml(text);
 
-        const escapedText =
-            this.escapeXml(text);
+        // Remove all existing <a:t> contents across paragraphs
+        // Put the new full text into the first <a:t> and clear the rest
+        let firstReplaced = false;
 
-        const regex =
-            /(<a:t>)[\s\S]*?(<\/a:t>)/;
-
-        this.xml =
-            this.xml.replace(
-                regex,
-                `$1${escapedText}$2`
-            );
+        this.xml = this.xml.replace(/<a:t>[\s\S]*?<\/a:t>/g, () => {
+            if (!firstReplaced) {
+                firstReplaced = true;
+                return `<a:t>${escapedText}</a:t>`;
+            }
+            return `<a:t></a:t>`;
+        });
     }
 
     getPosition() {
-
-        const match =
-            this.xml.match(
-                /<a:off\s+x="([^"]+)"\s+y="([^"]+)"/
-            );
-
-        if (!match) {
-            return null;
-        }
-
+        const match = this.xml.match(/<a:off\s+x="([^"]+)"\s+y="([^"]+)"/);
+        if (!match) return null;
         return {
             x: Number(match[1]),
             y: Number(match[2])
@@ -83,16 +51,8 @@ class PptxShape {
     }
 
     getSize() {
-
-        const match =
-            this.xml.match(
-                /<a:ext\s+cx="([^"]+)"\s+cy="([^"]+)"/
-            );
-
-        if (!match) {
-            return null;
-        }
-
+        const match = this.xml.match(/<a:ext\s+cx="([^"]+)"\s+cy="([^"]+)"/);
+        if (!match) return null;
         return {
             width: Number(match[1]),
             height: Number(match[2])
@@ -100,25 +60,18 @@ class PptxShape {
     }
 
     setPosition(x, y) {
-
-        this.xml =
-            this.xml.replace(
-                /(<a:off\s+x=")[^"]+(")\s+(y=")[^"]+(")/,
-                `$1${x}$2 $3${y}$4`
-            );
+        this.xml = this.xml.replace(
+            /(<a:off\s+x=")[^"]+(")\s+(y=")[^"]+(")/,
+            `$1${x}$2 $3${y}$4`
+        );
     }
 
     clone(newId) {
-
-        const clonedXml =
-            this.xml.replace(
-                /(<p:cNvPr\s+id=")[^"]+/,
-                `$1${newId}`
-            );
-
-        return new PptxShape(
-            clonedXml
+        const clonedXml = this.xml.replace(
+            /(<p:cNvPr\s+id=")[^"]+/,
+            `$1${newId}`
         );
+        return new PptxShape(clonedXml);
     }
 
     getXml() {
@@ -126,7 +79,6 @@ class PptxShape {
     }
 
     escapeXml(value) {
-
         return String(value)
             .replace(/&/g, "&amp;")
             .replace(/</g, "&lt;")
